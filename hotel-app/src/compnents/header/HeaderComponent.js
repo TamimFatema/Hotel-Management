48f6b1f304bd1d71ed405ee39a5f8510d70236eb
TamimFatema <1910807@iub.edu.bd>
2023-09-10 02:59:07 +0600

    updates

---
+++ hotel-app/src/compnents/header/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationCompnent from './NavigationCompnent'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <NavigationCompnent></NavigationCompnent>
+        </div>
+    )
+}
+
+export default HeaderComponent
---
+++ hotel-app/src/compnents/header/NavigationComponent.js
@@ -0,0 +1,16 @@
+import { Navbar, NavbarBrand } from 'reactstrap'
+import React from 'react'
+
+
+const NavigationCompnent = () => {
+    return (
+        <div>
+            <Navbar color="dark" dark>
+                <NavbarBrand href="/">Reactstrap</NavbarBrand>
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default NavigationCompnent
