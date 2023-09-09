c2ed76ac8887862dbf493c0f8e43f8450675b608
TamimFatema <1910807@iub.edu.bd>
2023-09-09 13:30:47 +0600

    login signup

--- hotel-booking/src/App.js
+++ hotel-booking/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from './components/Login/loginsignup';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
+      <Auth></Auth>
     </div>
   );
 }
