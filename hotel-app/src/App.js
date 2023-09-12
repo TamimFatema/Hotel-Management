b3c788fb55f054ddc4badf1fa0cc957eacf2864a
TamimFatema <1910807@iub.edu.bd>
2023-09-12 19:22:38 +0600

    navbar

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -1,12 +1,23 @@
+import React from 'react';
 import './App.css';
-import MainComponent from './components/MainComponent';
-
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRooms from "./pages/SingleRooms";
+import Error from "./pages/Home"
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <MainComponent></MainComponent>
-    </div>
+    <>
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRooms} />
+        <Route component={Error}></Route>
+      </Routes>
+      <Navbar></Navbar>
+    </>
   );
 }
 
